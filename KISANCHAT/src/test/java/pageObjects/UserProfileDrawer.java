b95816fe690b24890c0371ba2207857d7c9a1c14
Chetan Warke <chetan@kisan.net>
2018-06-20 18:49:35 +0530

    implementing edit user profile drawer page object

--- KISANCHAT/src/test/java/pageObjects/UserProfileDrawer.java
+++ KISANCHAT/src/test/java/pageObjects/UserProfileDrawer.java
@@ -1,5 +1,6 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import utility.BaseClass;
@@ -10,4 +11,25 @@ public class UserProfileDrawer extends BaseClass{
 		super(driver);
 	}
 
+	By closeBtn = By.xpath("//a[@class='closeBtn']");
+	By profileImg = By.xpath("//img[@alt='profile_pic']");
+	By userName = By.xpath("//div/h4[@class='mrgn-t-lg mrgn-b-xs font-bold-three ng-star-inserted']");
+	By editProfileBtn = By.cssSelector("#showUserProfileSideNav > app-show-user-profile > div > div.profile_heading > div.plueCircle.pad-t-none.text-center > a > span > i");
+	By chnlList = By.cssSelector("");
+	By viewAll = By.cssSelector("");
+	
+	public void get_userName() {
+		waitTillElementPresent(userName);
+		String name = driver.findElement(userName).getText();
+	}
+	
+	public void click_closeBtn() {
+		waitTillElementPresent(closeBtn);
+		driver.findElement(closeBtn).click();
+	}
+	
+	public void click_editProfileBtn() {
+		waitTillElementPresent(editProfileBtn);
+		driver.findElement(editProfileBtn).click();
+	}
 }
